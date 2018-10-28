9c4f5371d09372e4a894f15fc96bf84a6eb49050
MasterMedo <mislav.vuletic@gmail.com>
2018-10-28 06:28:42 +0100

    refactor: twist-lock.sh readability

--- twist-lock.sh
+++ twist-lock.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # dependencies: i3lock, scrot, imagemagick, xorg-xbacklight (xbacklight)
 
-start=`date +%s%N`
 # choose where the image will be temporarily stored
 img=/tmp/i3lock.png
 
@@ -9,16 +8,16 @@ img=/tmp/i3lock.png
 scrot -z $img # 0.205 sec
 
 # possible effects that can be applied to the image
-sample="sample 352x240 -sample 1920x1080" 									# 0.262 sec
-pixelate="scale 10% -scale 1000%" 													# 0.277 sec
-spread="spread 5" 																					# 1.012 sec
-swirl="bordercolor Black -border 100 -swirl 300 -shave 100"	# 1.085 sec
-blur="blur 18,5" 																						# 1.514 sec
-accent="-contrast-stretch 2%" 															# always on
-darken="-fill black -colorize 25%"													# always on
+sample="sample 352x240 -sample 1920x1080" # 0.262 sec
+pixelate="scale 10% -scale 1000%" # 0.277 sec
+spread="spread 5" # 1.012 sec
+swirl="bordercolor Black -border 100 -swirl 300 -shave 100" # 1.085 sec
+blur="blur 18,5" # 1.514 sec
+accent="-contrast-stretch 2%" # always on
+darken="-fill black -colorize 25%" # always on
 
 # list of chosen effects to be applied to the image
-effects=(	"${sample}" "${pixelate}" "${spread}" "${swirl}" "${blur}" ) # all effects
+effects=( "${sample}" "${pixelate}" "${spread}" "${swirl}" "${blur}" ) # all effects
 # effects=( "${sample}" "${pixelate}" ) # fast effects
 
 # random effect from the list of chosen effects
@@ -35,7 +34,7 @@ screen_brightness=$(xbacklight -get) # 0.00225 sec
 xbacklight -dec 80 & # 0.00063 sec
 
 # create the image overlay and locking the screen
-i3lock -nuef -i $img # [0.05, 0.08] sec, depends on size
+i3lock -nuef -i $img # [0.05, 0.08] sec, depends on image size
 
 # return screen brightness to what it was
 xbacklight -set $screen_brightness -time 500 &
