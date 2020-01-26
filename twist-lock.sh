80e1eb24d20f01ddf86f8e1bfe73ed557b6ce410
MasterMedo <mislav.vuletic@gmail.com>
2020-01-26 20:18:54 +0100

    swap: scrot for maim, add: colorize option

--- twist-lock.sh
+++ twist-lock.sh
@@ -5,27 +5,27 @@
 img=/tmp/i3lock.png
 
 # take screenshot
-scrot -z $img
+maim -B $img
 
 # possible effects that can be applied to the image
 sample="sample 352x240 -sample 1920x1080"
 pixelate="scale 10% -scale 1000%"
 spread="spread 5"
 swirl="bordercolor Black -border 100 -swirl 380 -implode 0.3 -shave 100"
-blur="filter Gaussian -resize 50% -define filter:sigma=2.5 -resize 200%"
+blur="filter Gaussian -resize 50% -define filter:sigma=3 -resize 200%"
 accent="-contrast-stretch 2%"
-darken="-fill black -colorize 25%"
+colorize="-fill orange -colorize 25%"
 
 # list of chosen effects to be applied to the image
 effects=( "${sample}" "${pixelate}" "${spread}" "${swirl}" "${blur}" ) # all effects
 #effects=( "${sample}" "${pixelate}" ) # fast effects
-#effects=( "${sample}" ) # test effects
+#effects=( "${swirl}" ) # test effects
 
 # random effect from the list of chosen effects
 effect=-$(shuf -n1 -e "${effects[@]}")
 
 # apply effect on the image
-convert $img $effect $accent $darken $img
+convert $img $effect $accent $colorize $img
 
 # get current screen brightness
 screen_brightness=$(xbacklight -get)
